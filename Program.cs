f2eff8a273ed823ca8b731cbb579789fef54431d
Joel Evans <joel.e@cwi.studio>
2022-09-11 10:51:36 -0400

    First commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Channels;
+
+var channel_options = new BoundedChannelOptions(1)
+{
+    SingleReader = true,
+    SingleWriter = true,
+    FullMode = BoundedChannelFullMode.Wait
+};
+var channel = Channel.CreateBounded<char>(channel_options);
+
+var producer = Task.Run(async () =>
+{
+    await channel.Writer.WriteAsync('a');
+    await channel.Writer.WriteAsync('b');   //commenting this will avoid a deadlock
+});
+
+var consumer = Task.Run(() =>
+{
+    channel.Reader.TryRead(out _);
+});
+
+await Task.WhenAll(producer, consumer);
+
+Console.WriteLine("Finished!");
